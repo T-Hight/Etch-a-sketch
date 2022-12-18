94763af0660a481eebf9f3b219375b0d179a9740
Taylor T Hight <Taylorthight@gmail.com>
2022-12-18 14:39:24 -0900

    List needed functions

--- Etch-a-sketch.js
+++ Etch-a-sketch.js
@@ -18,17 +18,6 @@ sketchBox.appendChild(sketch);
 
 const footer = document.querySelector('.footer');
 
-function makeRows(rows, cols) {
-    sketch.style.setProperty('--grid-rows', rows);
-    sketch.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement('div');
-        sketch.appendChild(cell).className = 'grid-item';
-    };
-};
-
-makeRows(16, 16);
-
 const colorBtn = document.querySelector('.color-btn');
 sideBar.appendChild(colorBtn);
 
@@ -43,3 +32,33 @@ sideBar.appendChild(slideContainer);
 
 const resetBtn = document.querySelector('.reset-btn');
 sideBar.appendChild(resetBtn);
+
+const slider = document.getElementById('myRange');
+const output = document.getElementById('value');
+
+function makeRows(rows, cols) {
+    sketch.style.setProperty('--grid-rows', rows);
+    sketch.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        sketch.appendChild(cell).className = 'grid-item';
+    };
+};
+
+makeRows(16, 16);
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+//Create function to change the color of grid squares when hovered over
+
+//Create function to change grid squares to random colors
+
+//Create function to choose the color of grid change
+
+//Create function to change the size of the grid between 16 and 100
+
+//Create function to reset grid size to 16
